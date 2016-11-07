2762d436e46ff6bfacd796bfa506ffaa7f7379e0
STLK <stalkach86@gmail.com>
2016-11-07 20:24:53 +0500

    initial commit

---
+++ main/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <list>
+
+#include <SFML/Graphics.hpp>
+
+#include "source/level.h"
+#include "source/view.h"
+
+
+int main()
+{
+
+	RenderWindow window(sf::VideoMode(800, 600), "Game");
+	view.reset(sf::FloatRect(0, 0, 800, 600));
+	window.setFramerateLimit(60);
+
+	float time;
+	Clock clock;
+
+	while (window.isOpen())
+	{
+		time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time = time / 800;
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+
+		window.setView(view);
+		window.clear();
+		//window.draw();
+		window.display();
+	}
+
+	return 0;
+}
---
+++ source/view.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+sf::View view;
+
+inline void setPlayerViewCoordinates(float x, float y)
+{
+	float tempX = x;
+	float tempY = y;
+
+	//TODO добавить ограничения по перемещению камеры
+	//if (x < 320) tempX = 320;
+	//if (y < 240) tempY = 240;
+	if (y > 624) tempY = 624;
+
+	view.setCenter(tempX, tempY);
+}
+
+
+///////////Перемещение камеры отдельно от персонажа/////////////
+
+inline void viewmap(float time) //
+{
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+	{
+		float x = view.getCenter().x;
+		float y = view.getCenter().y;
+
+		setPlayerViewCoordinates(x + 0.1 * time, y);
+	}
+
+	if (Keyboard::isKeyPressed(Keyboard::Down))
+	{
+		float x = view.getCenter().x;
+		float y = view.getCenter().y;
+
+		setPlayerViewCoordinates(x, y + 0.1 * time);
+	}
+
+	if (Keyboard::isKeyPressed(Keyboard::Left))
+	{
+		float x = view.getCenter().x;
+		float y = view.getCenter().y;
+
+		setPlayerViewCoordinates(x - 0.1 * time, y);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Up))
+	{
+		float x = view.getCenter().x;
+		float y = view.getCenter().y;
+
+		setPlayerViewCoordinates(x, y - 0.1 * time);
+	}
+}
+
+void viewByMouse(RenderWindow& window, float time)
+{
+	//TODO сделать перемещение вверх и вниз
+	Vector2i localPos = Mouse::getPosition(window);
+
+	if (localPos.x > window.getSize().x - 30)
+	{
+		float x = view.getCenter().x;
+		float y = view.getCenter().y;
+
+		setPlayerViewCoordinates(x + 0.1 * time, y);
+	}
+	if (localPos.x < 30)
+	{
+		float x = view.getCenter().x;
+		float y = view.getCenter().y;
+
+		setPlayerViewCoordinates(x - 0.1 * time, y);
+	}
+}
